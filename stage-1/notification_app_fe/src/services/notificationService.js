3696ab058e508728391ca6dab0e609da528cac58
manasamurakonda <murakondamanasa@gmail.com>
2026-06-05 11:22:19 +0530

    almost done with stage-1

--- stage-1/notification_app_fe/src/services/notificationService.js
+++ stage-1/notification_app_fe/src/services/notificationService.js
@@ -5,20 +5,19 @@ const TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 
 export const fetchNotifications = async () => {
   try {
-    console.log("API URL:", API_URL);
-    console.log("TOKEN:", TOKEN);
-
     const response = await axios.get(API_URL, {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
       },
     });
 
-    console.log("FULL RESPONSE:", response.data);
+    console.log("SUCCESS RESPONSE:", response.data);
 
     return response.data.notifications || [];
   } catch (error) {
-    console.error("API ERROR:", error);
+    console.error("ERROR RESPONSE:", error.response?.data);
+    console.error("STATUS:", error.response?.status);
+
     return [];
   }
 };
